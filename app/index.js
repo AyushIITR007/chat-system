65d68cd134eccebd979069e5e79ad4612e790d07
SudGupta <sud140598@gmail.com>
2022-08-06 12:25:01 +0530

    minor formatting

--- app/index.js
+++ app/index.js
@@ -19,11 +19,28 @@ function makeDoubleDigitIfSingle(num)
   return (num/10) < 1 ? "0" + num : num;
 }
 
+function AmPmFormatting(time)
+{
+  var hours = Number(time.substring(0,2));
+  if (hours > 12)
+  {
+    hours -= 12;
+    time += "pm";
+  }
+  else if (hours == 12)
+  {
+    time += "pm";
+  }
+  time[0] = (hours/10)%1;
+  time[1] = hours%10;
+  return time;
+}
+
 messageForm[0].addEventListener('submit', function(e) {
   e.preventDefault();
   const currdatetime = new Date(Date.now());
   var dateForDisplay = "[" + makeDoubleDigitIfSingle(currdatetime.getDay()) + "/" + makeDoubleDigitIfSingle(currdatetime.getMonth()) + "/" + (currdatetime.getFullYear() - 2000) + "]";
-  var timeForDisplay = makeDoubleDigitIfSingle(currdatetime.getHours()) + ":" + makeDoubleDigitIfSingle(currdatetime.getMinutes());
+  var timeForDisplay = AmPmFormatting(makeDoubleDigitIfSingle(currdatetime.getHours()) + ":" + makeDoubleDigitIfSingle(currdatetime.getMinutes()));
   if (messageBox[0].value) {
     var data = JSON.stringify({
       message: messageBox[0].value.trim(),
